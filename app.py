f1af496ea504b2b174c6cfbd2b88bdae975be8c3
David <66866223+david-prv@users.noreply.github.com>
2022-06-12 13:25:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import time
 from decouple import config
+from lib import argparser
 from lib import monitor
 from lib import collections
 
@@ -13,10 +14,14 @@ all subroutines together and run them. It's the main
 function of the whole monitoring service, so to say.
 """
 def runDaemon():
-    runner = monitor.Monitor(collections.COMPONENTS, collections.HOSTNAMES)
+    options = argparser.Argparser()
+    options.parse()
+
+    runner = monitor.Monitor(collections.COMPONENTS, collections.HOSTNAMES, options)
     while(True):
         runner.runAll()
-        time.sleep(int(config('DELAY')))
+        if options.repetitive: time.sleep(int(config('DELAY')))
+        else: exit("Done.")
 
 if __name__ == "__main__":
     runDaemon()
