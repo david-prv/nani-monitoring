9dddcb0f0f89ee59d136086e248c6233eebc5d79
David <66866223+david-prv@users.noreply.github.com>
2022-06-12 13:25:39 +0200

    Update mailclient.py

--- lib/mailclient.py
+++ lib/mailclient.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from pickletools import optimize
 import smtplib
 from decouple import config
 from colorama import Fore
@@ -13,23 +14,24 @@ email clients. We adjusted it for our purposes.
 It is not generalized.
 """
 class Emailer:
-    def __init__(self, sender, secret):
+    def __init__(self, sender, secret, suppress):
         self._sender = sender
         self._secret = secret
+        self.suppress = suppress
         self.error = 0
     
     def sendEmail(self, status, content, receiver):
         body = 'From: {}\nTo: {}\nSubject: {}\n\n{}'.format(self._sender, receiver, status, content)
-
-        print(f"{self._sender} -> {receiver}: [{Fore.GREEN if status == 'UP' else Fore.RED}{status}{Style.RESET_ALL}] {content}")
 
         try:
             smtp = smtplib.SMTP(config('SMTP_SERVER'), config('SMTP_PORT'))
             smtp.ehlo()
             smtp.starttls()
             smtp.login(self._sender, self._secret)
-            smtp.sendmail(self._sender, receiver, body)
+            if not self.suppress:
+                smtp.sendmail(self._sender, receiver, body)
+            print(("[Suppressed] " if self.suppress else "") + f"{self._sender} -> {receiver}: [{Fore.GREEN if status == 'UP' else Fore.RED}{status}{Style.RESET_ALL}] {content}")
             smtp.close()
         except Exception as err:
             self.error = 1
-            Exception(err)
+            print(f"Error! {err}")
